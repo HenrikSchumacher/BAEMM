1f11100c1c804d580043fbf158add2387215742c
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2023-02-15 11:56:05 +0100

    Ready for testing.

--- BAEMM_Test/main.cpp
+++ BAEMM_Test/main.cpp
@@ -101,34 +101,34 @@ using Complex       = std::complex<Real>;
 
 int main(int argc, const char * argv[])
 {
-//    using namespace Repulsor;
+    //    using namespace Repulsor;
     using namespace Tensors;
     using namespace Tools;
-//    using namespace BAEMM;
-
-  
+    //    using namespace BAEMM;
+    
+    
     
     //Loading vertex coordinates and simplices from file.
     
     const char * homedir = getenv("HOME");
-
+    
     if( homedir == nullptr)
     {
         homedir = getpwuid(getuid())->pw_dir;
     }
     std::string path ( homedir );
-
-//    std::string file_name = path + "/github/BAEMM/Meshes/TorusMesh_00153600T.txt";
+    
+    //    std::string file_name = path + "/github/BAEMM/Meshes/TorusMesh_00153600T.txt";
     std::string file_name = path + "/github/BAEMM/Meshes/TorusMesh_00038400T.txt";
-//    std::string file_name = path + "/github/BAEMM/Meshes/TorusMesh_00009600T.txt";
-//    std::string file_name = path + "/github/BAEMM/Meshes/TorusMesh_00000600T.txt";
-
-
+    //    std::string file_name = path + "/github/BAEMM/Meshes/TorusMesh_00009600T.txt";
+    //    std::string file_name = path + "/github/BAEMM/Meshes/TorusMesh_00000600T.txt";
+    
+    
     Tensor2<Real,Int> coords;
     Tensor2<Int ,Int> simplices;
     ReadFromFile(file_name, coords, simplices);
-
-
+    
+    
     // Clear file used by the profiler.
     Profiler::Clear( path );
     
@@ -136,7 +136,7 @@ int main(int argc, const char * argv[])
     print("");
     
     int OMP_thread_count_0;
-    #pragma omp parallel
+#pragma omp parallel
     {
         OMP_thread_count_0 = omp_get_num_threads();
     }
@@ -144,53 +144,53 @@ int main(int argc, const char * argv[])
     
     
     int OMP_thread_count = 8;
-//    int OMP_thread_count = 1;
+    //    int OMP_thread_count = 1;
     omp_set_num_threads(OMP_thread_count);
-
+    
     valprint("number of OpenMP threads",OMP_thread_count);
-
+    
     print("");
     print("");
-
+    
     static constexpr Int wave_count       = 64;
     static constexpr Int wave_chunk_size  = 16;
     static constexpr Int wave_chunk_count = wave_count / wave_chunk_size;
-
-
+    
+    
     print("");
     print("Preparing Helmholtz classes");
     print("");
-
-    BAEMM::Helmholtz_CPU H_CPU (
-        coords.data(),    coords.Dimension(0),
-        simplices.data(), simplices.Dimension(0),
-        OMP_thread_count
-    );
     
-
+    BAEMM::Helmholtz_CPU H_CPU (
+                                coords.data(),    coords.Dimension(0),
+                                simplices.data(), simplices.Dimension(0),
+                                OMP_thread_count
+                                );
+    
+    
     // Create an object that handles GPU acces via Metal.
     
     // Some pool to handle reference-counted objects associated to Metal (namespace MTL).
-//    NS::SharedPtr<NS::AutoreleasePool> auto_pool
-//        = NS::TransferPtr( NS::AutoreleasePool::alloc()->init() );
+    //    NS::SharedPtr<NS::AutoreleasePool> auto_pool
+    //        = NS::TransferPtr( NS::AutoreleasePool::alloc()->init() );
     
     // Request the GPU device.
-//    NS::SharedPtr<MTL::Device> device =
-//        reinterpret_cast<MTL::Device *>( MTL::CopyAllDevices()->object(0) )
-//    );
+    //    NS::SharedPtr<MTL::Device> device =
+    //        reinterpret_cast<MTL::Device *>( MTL::CopyAllDevices()->object(0) )
+    //    );
     
     NS::SharedPtr<MTL::Device> device = NS::TransferPtr(
-        reinterpret_cast<MTL::Device *>( MTL::CopyAllDevices()->object(0) )
-    );
+                                                        reinterpret_cast<MTL::Device *>( MTL::CopyAllDevices()->object(0) )
+                                                        );
     
     BAEMM::Helmholtz_Metal H_Metal (
-        device,
-        coords.data(),              // pointer to an array of doubles or floats
-        coords.Dimension(0),        // number of vertices
-        simplices.data(),           // pointer to an array of ints or long ints
-        simplices.Dimension(0),     // number of simplices
-        OMP_thread_count            // number of OpenMP threads to use.
-    );
+                                    device,
+                                    coords.data(),              // pointer to an array of doubles or floats
+                                    coords.Dimension(0),        // number of vertices
+                                    simplices.data(),           // pointer to an array of ints or long ints
+                                    simplices.Dimension(0),     // number of simplices
+                                    OMP_thread_count            // number of OpenMP threads to use.
+                                    );
     
     // Some matrices to hold data.
     Tensor2<Complex,Int> X     ( H_Metal.VertexCount(), wave_count );
@@ -203,13 +203,9 @@ int main(int argc, const char * argv[])
     // X.Read( some_buffer );
     
     
-    // Two real factors
-    const Complex alpha = 1;
-    const Complex beta  = 0;
-    
     // Prepare wave numbers (all equal to 2 in this example).
     constexpr Real kappa = 2.;
-
+    
     Tensor1<Real,Int> kappa_list (wave_chunk_count, kappa);
     
     // Set the coefficients for the operators
@@ -222,9 +218,25 @@ int main(int argc, const char * argv[])
         coeff_list[k][3] = Complex(1.0f,0.5f); // coefficient of adjoint double layer op
     }
     
+    
+    // Two complex factors
+    const Complex alpha = 1; // typically alpha = 1
+    const Complex beta  = 0; // beta  = 0 means "overwrite Y", beta = 1  means "add-into Y"
+    
+    
     const Int ldX = X.Dimension(1);
     const Int ldY = Y.Dimension(1);
 
+    const Complex * X_ptr = X.data();
+          Complex * Y_ptr = Y.data();
+    const Real    * kappa_ptr = kappa_list.data();
+    const Complex * coeff_ptr = coeff_list.data();
+    
+    // Compute Y = alpha * A * X + beta * Y
+    // where A =   coeff[0] * [mass]
+    //           + coeff[1] * [single layer op]
+    //           + coeff[2] * [double layer op]
+    //           + coeff[3] * [adjoint double layer op]
     
     H_CPU.ApplyBoundaryOperators_PL(
         alpha,
@@ -239,21 +251,31 @@ int main(int argc, const char * argv[])
         wave_chunk_size     // do it in chunks of size wave_chunk_size
     );
 
-    // Compute Y = alpha * A * X + beta * Y
-    // where A =   coeff[0] * [mass]
-    //           + coeff[1] * [single layer op]
-    //           + coeff[2] * [double layer op]
-    //           + coeff[3] * [adjoint double layer op]
+    
+
+    
+    // IMPORTANT:
+    // 1. Real and Complex have to be of same precision
+    // 2. type Real has to be real
+    // 3. type Complex has to be complex
+    //      e.g. Real = float and Complex = std::complex<float> would work.
+    //      e.g. Real = double and Complex = std::complex<double> would work, too.
+    // 4. X_ptr is array of size >= vertex_count * ldX
+    // 5. Y_ptr is array of size >= vertex_count * ldY
+    // 6. kappa_ptr is an array of size (wave_count + wave_chunk_size - 1) / wave_chunk_size
+    // 7. coeff_ptr is an array of size 4 * (wave_count + wave_chunk_size - 1) / wave_chunk_size.
+    // 8. wave_count <= ldX, ldY
+    // 9. wave_chunk_size = 16 or a nearby power of 2 should work best.
     
     H_Metal.ApplyBoundaryOperators_PL(
         alpha,
-        X.data(),           // pointer to complex floating point type
+        X_ptr,              // pointer to complex floating point type
         ldX,                // "leading dimension": number of columns in buffer X
         beta,
-        Y.data(),           // pointer to complex floating point type
+        Y_ptr,              // pointer to complex floating point type
         ldY,                // "leading dimension": number of columns in buffer Y
-        kappa_list.data(),  // the wave numbers to use; 1 for each chunk
-        coeff_list.data(),  // the coefficients to use; 4 for each chunk
+        kappa_ptr,          // the wave numbers to use; 1 for each chunk
+        coeff_ptr,          // the coefficients to use; 4 for each chunk
         wave_count,         // how many right hand sides to process
         wave_chunk_size     // do it in chunks of size wave_chunk_size
     );
@@ -315,7 +337,7 @@ int main(int argc, const char * argv[])
         coeff_2[k][2] = Complex(0.7f,1.1f);
         coeff_2[k][3] = Complex(1.2f,0.9f);
     }
-
+    
     print("");
     print("");
     print("Checking dependence of runtime on number of nonzero coeffiencts.");
