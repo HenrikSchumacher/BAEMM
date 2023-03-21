82e88a4b0da686e45707a26b086b33aa308706e1
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2023-03-21 11:52:00 +0100

    ApplyOperators.hpp

--- src/Helmholtz_Common/ApplyOperators.hpp
+++ src/Helmholtz_Common/ApplyOperators.hpp
@@ -1,7 +1,7 @@
 public:
 
     template<typename R_ext, typename C_ext, typename I_ext>
-    void ApplyOperators_PL(
+    void ApplyAvOp_PL(
         const C_ext alpha, const B_ext * B_in,  const I_ext ldB_in,
         const C_ext beta,        C_ext * C_out, const I_ext ldC_out,
         const I_ext cols
@@ -18,6 +18,29 @@ public:
         AvOp.Dot(
             Scalar::One<Complex>,  B_in,  ldB_in,
             Scalar::Zero<Complex>, C_out, ldC_out,
+            cols
+        );
+
+    }
+
+    template<typename R_ext, typename C_ext, typename I_ext>
+    void ApplyAvOpTransp_PL(
+        const C_ext alpha, const B_ext * B_in,  const I_ext ldB_in,
+        const C_ext beta,        C_ext * C_out, const I_ext ldC_out,
+        const I_ext cols
+    )
+    {
+        // Computes
+        //
+        //     C_out = alpha * A^T * B_in + beta * C_out,
+        //
+        // where A is the averaging operator and
+        // where B_in and C_out out are matrices of size simplex_count x cols and
+        // vertex_count x cols, respectively.
+     
+        AvOpTransp.Dot(
+            alpha, B_in,  ldB_in,
+            beta,  C_out, ldC_out,
             cols
         );
 
