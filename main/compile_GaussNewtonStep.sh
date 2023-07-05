17ed0012901d5664c917bd8ed14ba40b0410dc96
roensch <j.roensch@math.uni-goettingen.de>
2023-07-05 13:59:00 +0200

    backed up with degree of optimisation

--- main/compile_GaussNewtonStep.sh
+++ main/compile_GaussNewtonStep.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -g -O3 -DNDEBUG -std=c++20 -Wno-deprecated -I/HOME1/users/guests/jannr/github/BAEMM -I/HOME1/users/guests/jannr/anaconda3/envs/BEM/include -L/HOME1/users/guests/jannr/anaconda3/envs/BEM/lib -lOpenCL -lopenblas GaussNewtonStep.cpp -o /HOME1/users/guests/jannr/BEM/GaussNewton
+g++ -g -Ofast -DNDEBUG -std=c++20 -Wno-deprecated -I/HOME1/users/guests/jannr/github/BAEMM -I/HOME1/users/guests/jannr/anaconda3/envs/BEM/include -L/HOME1/users/guests/jannr/anaconda3/envs/BEM/lib -lOpenCL -lopenblas GaussNewtonStep.cpp -o /HOME1/users/guests/jannr/BEM/GaussNewton
