9af06a48c5ed5f21dd0fe3b8d54ac72eed2a2063
roensch <j.roensch@math.uni-goettingen.de>
2023-07-10 09:19:06 +0200

    compile flag changes

--- main/compile_GaussNewtonStep_TPM0.sh
+++ main/compile_GaussNewtonStep_TPM0.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -g -O3 -DNDEBUG -std=c++20 -Wno-deprecated -pthread -I/HOME1/users/guests/jannr/github/BAEMM -I/HOME1/users/guests/jannr/anaconda3/envs/BEM/include -L/HOME1/users/guests/jannr/anaconda3/envs/BEM/lib -lOpenCL -lopenblas GaussNewtonStep_TPM0.cpp -o /HOME1/users/guests/jannr/BEM/GaussNewton_TPM0
+g++ -O3 -DNDEBUG -std=c++20 -Wno-deprecated -pthread -I/HOME1/users/guests/jannr/github/BAEMM -I/HOME1/users/guests/jannr/anaconda3/envs/BEM/include -L/HOME1/users/guests/jannr/anaconda3/envs/BEM/lib -lOpenCL -lopenblas GaussNewtonStep_TPM0.cpp -o /HOME1/users/guests/jannr/BEM/GaussNewton_TPM0
