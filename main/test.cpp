ebc483768dfa68eca1c0a424d10d7c4d92503f7d
roensch <j.roensch@math.uni-goettingen.de>
2023-07-21 15:54:19 +0200

    tests

--- main/test.cpp
+++ main/test.cpp
@@ -60,7 +60,7 @@ int main()
 
     for (int i = 0 ; i < wave_chunk_count; i++)
     {
-        kappa[i] = Scalar::Pi<Real>;
+        kappa[i] = 1.0f;
     }
 
     // Real* C = (Real*)malloc(3 * n * sizeof(Real));
