5523079f675aa0524cd7c69bc77afd3c8f6d4357
roensch <j.roensch@math.uni-goettingen.de>
2023-07-21 14:22:12 +0200

    tests

--- main/PyTest.py
+++ main/PyTest.py
@@ -177,10 +177,9 @@ ret = calc_DFF_adj(connectivity,vertices,np.pi,incident_directions,measurement_d
 
 test_real = np.loadtxt("/HOME1/users/guests/jannr/github/BAEMM/main/data_real.txt").transpose()
 # test_imag = np.loadtxt("/HOME1/users/guests/jannr/github/BAEMM/main/data_imag.txt").transpose()
-print(ret[:,0:10])
-print(test_real[:,0:10])
-res = ret - test_real #- 1j *test_imag[0:4,:]
 
+res = ret - test_real #- 1j *test_imag[0:4,:]
+print(res[:,0:100])
 
 error = np.divide(np.amax(np.abs(res),axis = 1),np.amax(np.abs(ret),axis = 1))
 # error = np.divide(np.linalg.norm(res),np.linalg.norm(ret))
