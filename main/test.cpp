216ddef311405f1473530e52ba01cfa8212c2715
roensch <j.roensch@math.uni-goettingen.de>
2023-07-21 15:37:21 +0200

    tests

--- main/test.cpp
+++ main/test.cpp
@@ -39,16 +39,16 @@ int main()
 
     Real * kappa = (Real*)malloc(wave_chunk_count * sizeof(Real));
     Real* inc = (Real*)malloc(wave_chunk_size * 3 * sizeof(Real));
-    // Complex * coeff = (Complex*)malloc(4 * wave_chunk_count * sizeof(Complex));
+    Complex * coeff = (Complex*)malloc(4 * wave_chunk_count * sizeof(Complex));
     Complex * wave_coeff = (Complex*)malloc(4 * wave_chunk_count * sizeof(Complex));
 
-    // for(Int i = 0 ; i < wave_chunk_count ; i++)
-    // {
-    //     coeff[4 * i + 0] = 0.0f;
-    //     coeff[4 * i + 1] = 0.0f;
-    //     coeff[4 * i + 2] = 0.0f;
-    //     coeff[4 * i + 3] = 1.0f;
-    // }
+    for(Int i = 0 ; i < wave_chunk_count ; i++)
+    {
+        coeff[4 * i + 0] = 0.0f;
+        coeff[4 * i + 1] = 1.0f;
+        coeff[4 * i + 2] = 0.0f;
+        coeff[4 * i + 3] = 0.0f;
+    }
 
     for(Int i = 0 ; i < wave_chunk_count ; i++)
     {
@@ -150,7 +150,7 @@ int main()
     free(C);
     free(inc);
     free(kappa);
-    // free(coeff);
+    free(coeff);
     free(wave_coeff);
     return 0;
 }
