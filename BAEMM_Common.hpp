fbd93a557b543c567a518d7b5eaded16fcfc6fd9
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2024-07-22 15:15:03 +0200

    WaveType should be known by all Helmholtz classes. Moved it to the name space BAEMM.

---
+++ BAEMM_Common.hpp
@@ -0,0 +1,12 @@
+namespace BAEMM
+{
+    using namespace Tools;
+    using namespace Tensors;
+    using namespace Repulsor;
+    
+    enum class WaveType : unsigned char
+    {
+        Plane  = 201,
+        Radial = 202
+    };
+}
