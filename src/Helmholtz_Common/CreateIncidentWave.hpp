467da2ac8a61e5eb4d23cc3bf0fedf2bf184a939
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2024-07-23 00:52:59 +0200

    Code formatting.

--- src/Helmholtz_Common/CreateIncidentWave.hpp
+++ src/Helmholtz_Common/CreateIncidentWave.hpp
@@ -1,54 +1,58 @@
 public:
 
-    template<typename R_ext, typename C_ext, typename I_ext>
-    void CreateIncidentWave_PL(
-        const C_ext alpha, cptr<R_ext> incident_directions,  const I_ext inc_count,
-        const C_ext beta,  mptr<C_ext> C_out,                const I_ext ldC_out,
-        const R_ext kappa_,
-        const C_ext coeff_0,
-        const C_ext coeff_1,
-        const C_ext coeff_2,
-        const C_ext coeff_3,
-        const I_ext wave_count_,
-        const I_ext wave_chunk_size_,
-        WaveType type = WaveType::Plane
-    )
-    {
-        // Computes
-        //
-        //     C_out = alpha * A * B_in + beta * C_out,
-        //
-        // where B_in and C_out out are matrices of size vertex_count x wave_count_ and
-        // represent the vertex values of  wave_count_ piecewise-linear functions.
-        // The operator A is a linear combination of several operators, depending on kappa:
-        //
-        // A =   coeff_0 * MassMatrix
-        //     + coeff_1 * SingleLayerOp[kappa]
-        //     + coeff_2 * DoubleLayerOp[kappa]
-        //     + coeff_3 * AdjDblLayerOp[kappa]
-        //
-        //
-        
-        ASSERT_INT(I_ext);
-        ASSERT_REAL(R_ext);
-        ASSERT_COMPLEX(C_ext);
-        
-        if (inc_count != wave_chunk_size_)
-        {
-            if (inc_count > wave_chunk_size_)
-            {
-                print("number of inident waves has to match the size of the wave chunks. excess incident directions will be ignored");
-            }
-            if (inc_count < wave_chunk_size_)
-            {
-                eprint("error: number of inident waves has to match the size of the wave chunks");
-            }
-        }
-
-        LoadCoefficients(kappa_,coeff_0,coeff_1,coeff_2,coeff_3,wave_count_,wave_chunk_size_,static_cast<R_ext>(four_pi));
-
-        CreateIncidentWave_PL( alpha, incident_directions, beta, C_out, ldC_out, type );
-    }
+// TODO: Is this one ever called? If not, we should discard it.
+//
+//    template<typename I_ext, typename R_ext, typename C_ext>
+//    void CreateIncidentWave_PL(
+//        const C_ext alpha, cptr<R_ext> incident_directions,  const I_ext inc_count,
+//        const C_ext beta,  mptr<C_ext> C_out,                const I_ext ldC_out,
+//        const R_ext kappa_,
+//        const C_ext coeff_0,
+//        const C_ext coeff_1,
+//        const C_ext coeff_2,
+//        const C_ext coeff_3,
+//        const I_ext wave_count_,
+//        const I_ext wave_chunk_size_,
+//        WaveType type = WaveType::Plane
+//    )
+//    {
+//        // Computes
+//        //
+//        //     C_out = alpha * A * B_in + beta * C_out,
+//        //
+//        // where B_in and C_out our are matrices of size vertex_count x wave_count_ and
+//        // represent the vertex values of  wave_count_ piecewise-linear functions.
+//        // The operator A is a linear combination of several operators, depending on kappa:
+//        //
+//        // A =   coeff_0 * MassMatrix
+//        //     + coeff_1 * SingleLayerOp[kappa]
+//        //     + coeff_2 * DoubleLayerOp[kappa]
+//        //     + coeff_3 * AdjDblLayerOp[kappa]
+//        //
+//        //
+//        
+//        
+//        ASSERT_INT(I_ext);
+//        ASSERT_REAL(R_ext);
+//        ASSERT_COMPLEX(C_ext);
+//        
+//        
+//        if (inc_count != wave_chunk_size_)
+//        {
+//            if (inc_count > wave_chunk_size_)
+//            {
+//                eprint("number of incident waves has to match the size of the wave chunks. excess incident directions will be ignored");
+//            }
+//            if (inc_count < wave_chunk_size_)
+//            {
+//                eprint("number of inident waves has to match the size of the wave chunks");
+//            }
+//        }
+//
+//        LoadCoefficients(kappa_,coeff_0,coeff_1,coeff_2,coeff_3,wave_count_,wave_chunk_size_,static_cast<R_ext>(four_pi));
+//
+//        CreateIncidentWave_PL( alpha, incident_directions, beta, C_out, ldC_out, type );
+//    }
 
     template<typename R_ext, typename C_ext, typename I_ext>
     void CreateIncidentWave_PL(
@@ -61,8 +65,6 @@ public:
         WaveType type = WaveType::Plane
     )
     {
-        //  The same as above, but with several wave numbers kappa_list and several coefficients.
-
         ASSERT_INT(I_ext);
         ASSERT_REAL(R_ext);
         ASSERT_COMPLEX(C_ext);
@@ -84,7 +86,7 @@ public:
         CreateIncidentWave_PL( alpha, incident_directions, beta, C_out, ldC_out, type );
     }
 
-// creates wave_count incident waves for the scattering problem in the WEAK FORM
+    // Creates wave_count incident waves for the scattering problem in the WEAK FORM
     template<typename R_ext,typename C_ext, typename I_ext>
     void CreateIncidentWave_PL(
         const C_ext alpha, cptr<R_ext> incident_directions_,
@@ -92,15 +94,35 @@ public:
         WaveType type
     )
     {
-        // The same as above, but assumes that
+        // Computes
+        //
+        //     C_out = alpha * A * B_in + beta * C_out,
+        //
+        // where B_in and C_out our are matrices of size vertex_count x wave_count_ and
+        // represent the vertex values of  wave_count_ piecewise-linear functions.
+        // The operator A is a linear combination of several operators, depending on kappa:
+        //
+        // A =   coeff_0 * MassMatrix
+        //     + coeff_1 * SingleLayerOp[kappa]
+        //     + coeff_2 * DoubleLayerOp[kappa]
+        //     + coeff_3 * AdjDblLayerOp[kappa]
+        //
+        
         ASSERT_INT(I_ext);
+        ASSERT_REAL(R_ext);
         ASSERT_COMPLEX(C_ext);
 
-        ptic(ClassName()+"::CreateIncidentWave_PL");
+        std::string tag = ClassName() + "::CreateIncidentWave_PL"
+            + "<" + TypeName<I_ext>
+            + "," + TypeName<R_ext>
+            + "m" + TypeName<C_ext>
+            + ">";
+        
+        ptic(tag);
         
         if( wave_chunk_count < 1 )
         {
-            ptoc(ClassName()+"::CreateIncidentWave_PL");
+            ptoc(tag);
             return;
         }
         
@@ -116,8 +138,6 @@ public:
             Tensor2<Complex,Int> C ( simplex_count, ldC );
             
             incident_directions.Read(incident_directions_);
-            
-//            type_cast(incident_directions, incident_directions_, 3 * wave_chunk_size, 1);
 
             if (type == WaveType::Plane)
             {
@@ -128,12 +148,13 @@ public:
                 IncidentWaveKernel_Radial_C( kappa, c, incident_directions.data(), C.data() );
             }
 
-            // use transpose averaging operator to get from PC to PL boundary functions
+            // Use transpose averaging operator to get from PC to PL boundary functions
             AvOpTransp.Dot(
                 alpha, C.data(), ldC,
                 beta,  C_out,    ldC_out,
                 wave_count
             );
         }
-        ptoc(ClassName()+"::CreateIncidentWave_PL");
+        
+        ptoc(tag);
     }
