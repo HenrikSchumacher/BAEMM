d1323de900f7b6fbce162c7fe11d37b42b4e445e
HenrikSchumacher <24621470+HenrikSchumacher@users.noreply.github.com>
2024-07-27 14:37:49 +0200

    Old production code; used as ground truth for debugging.

--- Helmholtz_OpenCL.hpp
+++ Helmholtz_OpenCL.hpp
@@ -4,9 +4,18 @@
 
 #define TOOLS_ENABLE_PROFILER
 
-#include <complex>
-#include <cblas.h>
-#include <lapack.h>
+
+
+#ifdef __APPLE__
+/// Use these while on a mac. Don't forget to issue the compiler flag `-framework Accelerate`.
+///
+    #include "Accelerate/Accelerate.h"
+#else
+/// This should work for OpenBLAS.
+    #include <complex>
+    #include <cblas.h>
+    #include <lapack.h>
+#endif
 
 #include "submodules/Repulsor/Repulsor.hpp"
 
@@ -99,7 +108,17 @@ namespace BAEMM
 
             context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
 
-            command_queue = clCreateCommandQueueWithProperties(context, device_id, 0, &ret);
+#ifdef __APPLE__
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wdeprecated-declarations"
+            // Apple froze OpenCL support at version 1.2. Thus, the OpenCL 2.0
+            // feature clCreateCommandQueueWithProperties is not supported.
+            // Instead we can use this (deprecated) feature:
+            command_queue = clCreateCommandQueue(context,device_id,0,&ret);
+    #pragma clang diagnostic pop
+#else
+            command_queue = clCreateCommandQueueWithProperties(context,device_id,0,&ret);
+#endif
 
             // initialize the Opencl buffers and host pointers
             InitializeBuffers(simplex_count,meas_directions_);
@@ -158,7 +177,17 @@ namespace BAEMM
 
             context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
 
-            command_queue = clCreateCommandQueueWithProperties(context, device_id, 0, &ret);
+#ifdef __APPLE__
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wdeprecated-declarations"
+            // Apple froze OpenCL support at version 1.2. Thus, the OpenCL 2.0
+            // feature clCreateCommandQueueWithProperties is not supported.
+            // Instead we can use this (deprecated) feature:
+            command_queue = clCreateCommandQueue(context,device_id,0,&ret);
+    #pragma clang diagnostic pop
+#else
+            command_queue = clCreateCommandQueueWithProperties(context,device_id,0,&ret);
+#endif
 
             // initialize the Opencl buffers and host pointers
             InitializeBuffers(simplex_count,meas_directions_);
---
+++ ReadMeshFromFile.hpp
@@ -0,0 +1,70 @@
+namespace BAEMM
+{
+    template<typename Real, typename Int>
+    void ReadMeshFromFile(
+        cref<std::string>       file_name,
+        mref<Tensor2<Real,Int>> coords,
+        mref<Tensor2<Int, Int>> simplices
+    )
+    {
+        ptic("ReadMeshFromFile");
+        
+        print("Reading mesh from file "+file_name+".");
+        
+        std::ifstream s (file_name);
+        
+        if( !s.good() )
+        {
+            eprint("ReadFromFile: File "+file_name+" could not be opened.");
+            
+            return;
+        }
+        
+        std::string str;
+        Int amb_dim;
+        Int dom_dim;
+        Int vertex_count;
+        Int simplex_count;
+        s >> str;
+        s >> dom_dim;
+        dump(dom_dim);
+        s >> str;
+        s >> amb_dim;
+        dump(amb_dim);
+        s >> str;
+        s >> vertex_count;
+        dump(vertex_count);
+        s >> str;
+        s >> simplex_count;
+        dump(simplex_count);
+        
+        const Int simplex_size = dom_dim+1;
+        
+        dump(simplex_size);
+        
+        coords    = Tensor2<Real,Int>(vertex_count, amb_dim     );
+        simplices = Tensor2<Int, Int>(simplex_count,simplex_size);
+        
+        mptr<Real> V = coords.data();
+        mptr<Int>  S = simplices.data();
+        
+        for( Int i = 0; i < vertex_count; ++i )
+        {
+            for( Int k = 0; k < amb_dim; ++k )
+            {
+                s >> V[amb_dim * i + k];
+            }
+        }
+        
+        for( Int i = 0; i < simplex_count; ++i )
+        {
+            for( Int k = 0; k < simplex_size; ++k )
+            {
+                s >> S[simplex_size * i + k];
+            }
+        }
+        
+        ptoc("ReadMeshFromFile");
+    }
+    
+} // namespace BAEMM
