989f5f06f6bf04001ca4215a6ce8dbb6aafb05ef
roensch <j.roensch@math.uni-goettingen.de>
2024-07-30 10:13:09 +0200

    problems with path

--- Test_GaussNewton/main.cpp
+++ Test_GaussNewton/main.cpp
@@ -31,6 +31,7 @@ int main()
     std::filesystem::path this_file { __FILE__ };
     std::filesystem::path repo_dir = this_file.parent_path().parent_path();
     std::filesystem::path mesh_dir = repo_dir / "Meshes";
+    std::cout << mesh_dir << std::endl;
     std::filesystem::path home_dir = HomeDirectory();
         
 //    Profiler::Clear( home_dir );
